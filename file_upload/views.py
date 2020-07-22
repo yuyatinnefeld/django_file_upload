7a85fa185f97d451c84033aac5e25a9b2a687bd1
Yu Ya! <yuyawamo@gmail.com>
2020-07-22 16:16:46 +0000

    file upload template complete

--- file_upload/views.py
+++ file_upload/views.py
@@ -8,7 +8,7 @@ def fileUploadView(request):
 
 def fileUploadRun(request):
 
-    csv_info = {}
+    info = {}
 
     if request.method == 'POST':
         uploaded_file = request.FILES['filename'] 
@@ -19,11 +19,15 @@ def fileUploadRun(request):
         fs.save(uploaded_filename, uploaded_file)
         csv_file = f"./media/{uploaded_filename}"
         df = pd.read_csv(csv_file) # print(df)
-        urls = df[["URL"]].values
-        
-          
-    csv_info['url'] = urls
-    #urls[[1][0]]
+        url_list = []
+      
+        for url in df['URL'].unique():
+          #print(url)
+          url_list.append(url)
+               
 
-    return render(request,'file_upload_result.html',{'csv_info':csv_info})
+    info['URL'] = url_list
+    info['Category'] = {'brand','ecommerce','finace','ecommerce'}
+
+    return render(request,'file_upload_result.html',{'info':info})
     
