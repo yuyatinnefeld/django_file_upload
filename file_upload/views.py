d1897b370ae9ed7d11be6fd9b3ac9bd558129c7b
Yu Ya! <yuyawamo@gmail.com>
2020-07-22 12:14:01 +0000

    value update

--- file_upload/views.py
+++ file_upload/views.py
@@ -7,15 +7,23 @@ def fileUploadView(request):
     return render(request, 'file_upload.html')
 
 def fileUploadRun(request):
+
+    csv_info = {}
+
     if request.method == 'POST':
         uploaded_file = request.FILES['filename'] 
         uploaded_filename = uploaded_file.name
+        
         #save the uploaded file in the media forder
         fs = FileSystemStorage()
         fs.save(uploaded_filename, uploaded_file)
-        csv = f"./media/{uploaded_filename}"
-        df = pd.read_csv(csv)
-        print(df(2))
+        csv_file = f"./media/{uploaded_filename}"
+        df = pd.read_csv(csv_file) # print(df)
+        urls = df[["URL"]].values
+        
+          
+    csv_info['url'] = urls
+    #urls[[1][0]]
 
-    return render(request,'file_upload_result.html')    
+    return render(request,'file_upload_result.html',{'csv_info':csv_info})
     
